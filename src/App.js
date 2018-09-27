3c6f3491f6b359d5a153b2ab055d1fc5cc347d88
ginNyang2 <bbsnake10@gmail.com>
2018-09-27 19:48:24 +0900

    write reference information

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/* 누구든지 하는 리액트 4편: props와 state */
+//https://velopert.com/3629
+
 import React, { Component } from 'react';
 import MyName from './components/MyName';
 import Counter from './components/Counter';
